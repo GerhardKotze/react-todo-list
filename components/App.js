c843abe145b34f93d1507ca875a623bee63aa39c
GerhardKotze <gkotze@retrorabbit.co.za>
2017-08-14 20:53:59 +0200

    part 4 commit

--- components/App.js
+++ components/App.js
@@ -1,1 +1,9 @@
+import React, {Component} from 'react'
 
+class App extends Component {
+    render() {
+        return <div>THIS IS DEF A HOT (module reloading) REACT APP NOW </div>
+    }
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,34 @@
+//large javascript opject
+var webpack = require('webpack');
 
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './client/client.js'
+    ],
+    output: {
+        path: require('path').resolve('./dist'),
+        filename:  'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+        //needs to middleware within server
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                //use babel file in project directory or setup query
+                query: {
+                    presets: ['react','es2015','react-hmre'] //hot module reload 
+                }
+            }
+        ]
+    }
+}
